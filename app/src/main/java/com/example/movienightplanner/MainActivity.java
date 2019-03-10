c86aaf4dcce5acfd37ddf608f468638df2374ff8
Remi <s3399752@student.rmit.edu.au>
2019-03-10 19:44:43 +1100

    Second commit home

--- app/src/main/java/com/example/movienightplanner/MainActivity.java
+++ app/src/main/java/com/example/movienightplanner/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.movienightplanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button schedule_event;
+    private Button view_events;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        schedule_event = findViewById(R.id.schedule_button);
+
+        schedule_event.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CreateEvent.class);
+                startActivity(intent);
+            }
+        });
+
+        view_events = findViewById(R.id.view_button);
+
+        view_events.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ViewEvents.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
